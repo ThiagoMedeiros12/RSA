d1547b32bb4a35138cf6e6c5c8902f3eb7af4253
Thiago Medeiros <thia.med@hotmail.com>
2025-08-13 21:06:36 -0300

    configurado o botao, precisa revisar, importei as funçoes do arquivo logica.py

--- GUI.py
+++ GUI.py
@@ -1,16 +1,23 @@
 import tkinter as tk
+from logica import decript_msg as dm , criptografar_descriptografar as cm
 
-def on_click():
-    return endscreen_call()
+
 
 def endscreen_call():
     endscreen = tk.Toplevel()
     endscreen.title("endscreen")
-    endscreen_msg = tk.Label(endscreen,text ="Mensagem:")
-    endscreen_keys = tk.Label(endscreen,text="Chave:")
+    endscreen_msg = tk.Label(endscreen,text =f"Mensagem:"{msg})
+    endscreen_keys = tk.Label(endscreen,text =f"Chave:"{list_keys}")
 
     endscreen_msg.pack()
     endscreen_keys.pack()
+
+def on_click():
+    if button_cript['state'] == 'clicked':
+        input_msg = entrada_msg_cript.get()
+        list_keys, msg = cm(input=input_msg)
+    return endscreen_call()
+
 
 
 app = tk.Tk()
@@ -18,7 +25,7 @@ app = tk.Tk()
 greeting = tk.Label(app,text='Bem vindo ao meu app de criptografia RSA')
 ask_msg = tk.Label(app,text='Mensagem:')
 entrada_msg_cript = tk.Entry(app)
-button_cript = tk.Button(app,text="Criptografar", command=on_click )
+button_cript = tk.Button(app,text="Criptografar",name='Cript', command=on_click)
 ask_cript_key = tk.Label(app,text='Chave:')
 key = tk.Entry(app)
 ask_cript_msg = tk.Label(app,text='Mensagem:')
@@ -40,7 +47,6 @@ button_decript.pack(pady=10)
 
 
 app.title("CriptV0.1")
-
 
 
 
